91f710bafc66a850b5b1dc9e6cac0b59062974f1
GangOn0215 <ekdldktnekf@gmail.com>
2025-02-25 17:18:08 +0900

    INIT

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let name;
+	let count = 0;
+
+	console.log({ count });
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
