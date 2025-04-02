9a704fc3b1a0710cfd22a03ec3eeee5cf06e4fa1
GangOn0215 <ekdldktnekf@gmail.com>
2025-04-02 17:43:02 +0900

    [ Todo ] todolist 수정 할 수 있게  ( 아직 작업중 )

--- src/lib/components/todos/TodoListItem.svelte
+++ src/lib/components/todos/TodoListItem.svelte
@@ -3,18 +3,21 @@
 </script>
 
 <div class="todo-wrap" key={todo.id}>
-  <div class="todo-item">
-      <span class="todo-title">[ {todo.title} ]</span>
-      <span class="todo-content">{todo.content}</span> 
-  </div>
+  <!-- <div class="todo-item">
+    <span class="todo-title">[ {todo.title} ]</span>
+    <span class="todo-content">{todo.content}</span> 
+  </div> -->
+  <input type="text">
+  <!-- 만약 업데이트 버튼 누르면, 위 div.todo-item이 그냥 input이 되기를 원함. -->
+  
 </div>
 
 <style lang="scss">
   .todo-wrap {
       display: flex;
       justify-content: space-between;
-      width: 70%;
-      padding: 0.5rem;
+      width: 60%;
+      padding: 0.5rem;  
       border: 1px solid #ccc;
       border-radius: 0.25rem;
       
